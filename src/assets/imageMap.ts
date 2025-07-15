c37744dd30047cf87fcf5fe1d7b514b821a533f4
Diego Albuja <diegofalbuja@gmail.com>
2025-07-15 17:22:58 -0500

    add: alimentos saludables, galletas terminadas

--- src/assets/imageMap.ts
+++ src/assets/imageMap.ts
@@ -11,6 +11,10 @@ const imageMap: Record<string, any> = {
   '0000000000002': require('./images/0000000000002.png'),
   '0000000000003': require('./images/0000000000003.png'),
   '7862129010314': require('./images/7862129010314.png'),
+  '7702189048202': require('./images/7702189048202.png'),
+  '8412170027107': require('./images/8412170027107.png'),
+  '0000000000004': require('./images/0000000000004.png'),
+  '7622210873002': require('./images/7622210873002.png'),
 };
 
 export default imageMap;
